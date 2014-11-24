4b9142d8c6b68334d8826f466adcd2a35bcc2b07
Coywolf <budawolf333@gmail.com>
2014-11-24 10:39:22 -0500

    blocking out event javascript

--- GameVoting/Scripts/Event/Event.js
+++ GameVoting/Scripts/Event/Event.js
@@ -1,1 +1,25 @@
-﻿
+﻿var ViewModel = function (data) {
+    var self = this;
+};
+
+$(document).ready(function () {
+    var splitUrl = window.location.href.split('/');
+    var eventId = splitUrl[splitUrl.length - 1];  //get the last element, which should be the id
+
+    //make sure this is a valid number
+    if (!isNaN(eventId)) {
+        $.ajax({
+            url: 'GetEvent',
+            data: {
+                eventId: eventId
+            },
+            dataType: 'json',
+            success: function (data) {
+                ko.applyBindings(new ViewModel(data));
+            }
+        });
+    }
+    else {
+        //todo url parse error, redirect to index?
+    }
+});
