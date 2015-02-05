dc89598c41fce584b2138aea5b9e1b402ce37437
Michael Bush <michael.bush@inin.com>
2015-02-05 10:32:55 -0500

    Email notification when someone cheats

--- GameVoting/Helpers/EventExtensions.cs
+++ GameVoting/Helpers/EventExtensions.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DotNetOpenAuth.OpenId.Extensions.AttributeExchange;
 using GameVoting.Models.DatabaseModels;
 using GameVoting.Models.ViewModels;
+using WebMatrix.WebData;
 
 namespace GameVoting.Helpers
 {
@@ -11,35 +13,56 @@ namespace GameVoting.Helpers
     {
         public static string ValidateVotes(this Event e, List<EventOptionViewModel> votes)
         {
-            var errorMessage = "";
-
+            //Check that all options have a score
             if (votes.Count() != e.Options.Count || votes.Any(v => !v.Score.HasValue))
             {
-                errorMessage = "A score must be chosen for all options";
+                return "A score must be chosen for all options";
+            }
+
+            //Check uniqueness of selections, depending on the vote type
+            if (e.Type.Name == "Favorite")
+            {
+                if (votes.Count(v => v.Score.Value == 1) > 1)
+                {
+                    return "Only one option may be chosen as your favorite";
+                }
+            }
+            else if (e.Type.Name == "Rank")
+            {
+                if (votes.Select(v => v.Score.Value).Distinct().Count() != votes.Count())
+                {
+                    return "All options must be assigned a unique rank";
+                }
             }
             else
             {
-                if (e.Type.Name == "Favorite")
-                {
-                    if (votes.Count(v => v.Score.Value == 1) > 1)
-                    {
-                        errorMessage = "Only one option may be chosen as your favorite";
-                    }
-                }
-                else if (e.Type.Name == "Rank")
-                {
-                    if (votes.Select(v => v.Score.Value).Distinct().Count() != votes.Count())
-                    {
-                        errorMessage = "All options must be assigned a unique rank";
-                    }
-                }
-                else
-                {
-                    //No restriction on selections
-                }
+                //No restriction on selections
             }
 
-            return errorMessage;
+            var min = e.Type.MinScore ?? e.Options.Count;
+            var max = e.Type.MaxScore ?? e.Options.Count;
+
+            if (votes.Any(v => v.Score < min || v.Score > max))
+            {
+                try
+                {
+                    System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+                    message.To.Add("michael.bush@inin.com");
+                    message.Subject = "Cheater";
+                    message.From = new System.Net.Mail.MailAddress("michael.bush@inin.com");
+                    message.Body = WebSecurity.CurrentUserName;
+                    System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.inin.com");
+                    smtp.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+
+                return "Stop hacking the values. Bush knows who you are.";
+            }
+
+            return "";
         }
     }
 }
